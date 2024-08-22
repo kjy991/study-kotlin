bd9fa57f101279d2e3ac410444af769059be865b
yeopking <yeopking@optatumflatform.com>
2024-08-22 11:40:26 +0900

    hello-kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package org.example
 
-fun main() {
+fun main(args: Array<String>) {
+    val sum = sum(1, 3)
+    val number: Int = 15
+    println("sum : $sum")
     println("Hello World!")
+
+
+    println("Hello World! ${if (number > 10) number else "number is null"}")
+    println("sum World! ${sum(3,5)}")
 }
+
+fun sum(a: Int, b: Int): Int = a + b

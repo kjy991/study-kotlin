fa8f20d4e8b1807ba5240c50e1474bb1d09571ea
kjy991 <kjy991@gmail.com>
2024-08-24 21:14:04 +0900

    collection basic

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,5 +1,7 @@
 package org.example
 
+import Child
+
 data class Person(
     val name: String,
     val age: Int,
@@ -14,6 +16,15 @@ class Rectangle(val height: Int, val width: Int) {
 }
 
 
+val persons = listOf(
+    Person("a", 1, true),
+    Person("b", 2, true),
+    Person("c", 3, true),
+    Person("d", 4, true),
+    Person("e", 5, true),
+    Person("f", 6, true),
+)
+
 fun main(args: Array<String>) {
     val person = Person("yk", 34, false)
     println("person = $person")
@@ -21,19 +32,43 @@ fun main(args: Array<String>) {
     val square = Rectangle(10, 10)
     println(square.isSquare)
 
-
-    val persons = listOf(
-        Person("a", 1, true),
-        Person("b", 2, true),
-        Person("c", 3, true),
-        Person("d", 4, true),
-        Person("e", 5, true),
-        Person("f", 6, true),
-    )
-
     println(persons.maxOfOrNull({ person: Person -> person.age }))
-    println(persons.maxByOrNull ({ person: Person -> person.age }))
+    println(persons.maxByOrNull({ person: Person -> person.age }))
     println(persons.minByOrNull { it.age })
 
+    println("===============================")
+    println(persons
+        .filter { it.age > 3 }
+        .map { "${it.name}'s age is ${it.age} \n" }
+    )
+
+    println(persons
+        .filter { it.age > 3 }
+        .map { Child.ofDefaultAge(it.name, "no family") }
+//        .find { it.subName == "f" }
+        .count { it.age == 0 }
+//        .any { it.age < 1 }
+    )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
